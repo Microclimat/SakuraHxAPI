9f79cf788070012c3c9d3252f715e04a6e325faf
David Mouton <damo@tamina-online.com>
2016-04-05 16:10:17 +0200

    + DocumentType

--- src/com/sakura/api/SakuraEditor.hx
+++ src/com/sakura/api/SakuraEditor.hx
@@ -1115,6 +1115,16 @@ class UrlPair {
 
 }
 
+@:enum abstract DocumentType(Int) from Int to Int {
+    var ALBUM_PICTURE = 1;
+    var HD_RENDERING = 4;
+    var MULTIPLE = 9;
+    var PATTERN_PICTURE = 6;
+    var PRODUCT_VIEW = 5;
+    var CUSTOMER_DESIGN_THUMB = 8;
+    var USER_PICTURE = 7;
+}
+
 @:enum abstract HostName(String) from String to String  {
 
     var PREPROD = 'preprod';
