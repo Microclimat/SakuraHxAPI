53c6ec1be23ef0f53c290bcbfe6a39f0bba61acd
Yann Bergonzat <yannbergonzat@gmail.com>
2016-07-25 12:09:09 +0200

    5.1.7

--- src/com/sakura/api/model/design/CustomerDesign.hx
+++ src/com/sakura/api/model/design/CustomerDesign.hx
@@ -1,11 +1,13 @@
 package com.sakura.api.model.design;
+import com.sakura.api.model.net.Host.Version;
 class CustomerDesign extends Pattern {
 
     public var previewPicture:Picture;
-    public var price:Float = 0;
+    public var price:Float = 0.0;
+    public var version:Version = '';
 
 
-    public function new(id:Float = 0, name:String = "", description:String = "", enabled:Bool = true) {
+    public function new(id:Float = 0.0, name:String = "", description:String = "", enabled:Bool = true) {
         super(id,name,description,enabled);
     }
 }
